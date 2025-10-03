d67db288b9b60315a131d6ceeaff1dcc4b5fa205
Eileen Jiang <eileenj14@nycstudents.net>
2025-10-03 11:44:32 -0400

    Game start.

---
+++ src/Level.java
@@ -0,0 +1,14 @@
+public class Level
+{
+    /** Returns true if the player reached the goal on this level and returns false otherwise */
+    public boolean goalReached()
+    {
+        return true;
+    }
+
+    /** Returns the number of points (a positive integer) recorded for this level */
+    public int getPoints()
+    {
+        return 0;
+    }
+}
